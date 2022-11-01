4db0cd14ac74c26a212d305d93a1596342495327
Styrbjörn Nordberg <98731680+styrbjorn-n@users.noreply.github.com>
2022-11-01 16:27:17 +0100

    Update index.php

--- code/index.php
+++ code/index.php
@@ -4,7 +4,7 @@
     <div class="help_que">
         <h1>Sturdent list</h1>
         <div>
-            <input id="user_input" type="text">
+            <input class="user_input" id="user_input" type="text" onkeydown="enter(this)">
         </div>
         <div id="student_list"></div>
     </div>
