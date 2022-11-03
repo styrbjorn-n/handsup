6b671768527d45cc5fd6d852b1114fccc9d35328
Styrbjörn Nordberg <styris.n@gmail.com>
2022-11-04 00:55:54 +0100

    changed title

--- code/header.php
+++ code/header.php
@@ -28,6 +28,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
     <link rel="stylesheet" href="footer.css">
-    <title>handsup</title>
+    <title>handsup tool</title>
 </head>
 <body>
