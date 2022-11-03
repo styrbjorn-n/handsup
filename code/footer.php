91d6cbc955d9a447707ab866a03e2b5e7bd0f3a6
Styrbjörn Nordberg <styris.n@gmail.com>
2022-11-03 23:48:44 +0100

    added footer and more styling

--- code/footer.php
+++ code/footer.php
@@ -1,3 +1,7 @@
+<footer>
+    <p class="email">Contact: <?php echo $email; ?></p>
+</footer>
+
 <script src="student_list.js"></script>    
 </body>
 </html>
--- code/header.php
+++ code/header.php
@@ -17,6 +17,8 @@
 
         ],
     ];
+    $email = "stynor0915@skola.goteborg.se";
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,7 +26,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="index.css">
     <link rel="stylesheet" href="footer.css">
     <title>handsup</title>
